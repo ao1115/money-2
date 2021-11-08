728c48e39b40739163cf059cbc977e293b8c55dc
aoxiufang <1677120510@qq.com>
2021-11-08 16:14:18 +0800

    完善nav组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -11,14 +11,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper {
-  border: 1px solid red;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-.content {
-  overflow: auto;
-  flex-grow: 1; //把高度尽量的给它，剩下的是Nav的
-}
 </style>
