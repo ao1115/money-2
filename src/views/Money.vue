3c23cc76d92312360c872f3c0eb9b606255721be
aoxiufang <1677120510@qq.com>
2021-11-08 10:48:45 +0800

    创建三个页面

---
+++ src/views/Money.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>记账页面</div>
+</template>
+
+<script>
+export default {
+  name: "Monye",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <div>统计页面</div>
+</template>
+
+<script>
+export default {
+  name: "Statistics",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
