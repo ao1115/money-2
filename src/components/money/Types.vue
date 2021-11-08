a2a8c003118eddd03b9eae9c5b4626bbb3644e07
aoxiufang <1677120510@qq.com>
2021-11-08 20:29:09 +0800

    用js组件写收入支出

--- src/components/money/Types.vue
+++ src/components/money/Types.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
@@ -10,6 +10,19 @@
 <script>
 export default {
   name: "Types",
+  data() {
+    return {
+      type: "-", //'-'表示支出，'+'表示收入
+    };
+  },
+  methods: {
+    selectType(type) {
+      if (type !== "-" && type !== "+") {
+        throw new Error("type is unkown");
+      }
+      this.type = type;
+    },
+  },
 };
 </script>
 
