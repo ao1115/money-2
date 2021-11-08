f1f56784573a908fe4841c6aabc56126594f73e8
aoxiufang <1677120510@qq.com>
2021-11-08 14:04:13 +0800

    完成Money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    记账页面
+  <div class="nav-wrapper">
+    <div class="content">记账页面</div>
+
     <Nav />
   </div>
 </template>
@@ -12,4 +13,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  overflow: auto;
+  flex-grow: 1; //把高度尽量的给它，剩下的是Nav的
+}
 </style>
