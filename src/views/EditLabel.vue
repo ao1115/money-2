3477d97d50ce4e689796030deb9d312dc7769194
aoxiufang <1677120510@qq.com>
2021-11-12 20:41:51 +0800

    添加EditLabel的样式

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,9 +1,15 @@
 
 <template>
   <Layout>
-    <Icon name="left" />
-    <Notes fieldName="备注" placeholder="请输入备注" />
-    <div class="createTag-wrapper">
+    <div class="navBar">
+      <Icon class="leftIcon" name="left" />
+      <span class="title">编辑标签</span>
+      <span class="left"></span>
+    </div>
+    <div class="form-wrapper">
+      <Notes field-name="标签名" placeholder="请输入标签名" />
+    </div>
+    <div class="button-wrapper">
       <Button>删除标签</Button>
     </div>
   </Layout>
@@ -32,4 +38,32 @@ export default class Labels extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.navBar {
+  text-align: center;
+  font-size: 16px;
+  padding: 12px 16px;
+  background: white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  > .leftIcon {
+    width: 24px;
+    height: 24px;
+  }
+  > .rightIcon {
+    width: 24px;
+    height: 24px;
+  }
+}
+.form-wrapper {
+  background: white;
+  margin-top: 8px;
+  border: 1px solid red;
+}
+.button-wrapper {
+  text-align: center;
+  padding: 16px;
+  margin-top: 44-16px;
+}
 </style>
