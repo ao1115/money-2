84db5ab28132fb1e931b25811613328729ce5020
aoxiufang <1677120510@qq.com>
2021-11-12 17:26:35 +0800

    将tag的string改为id 和name

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,7 +1,12 @@
 const localStorageName = 'tagList'
+//将name作为id
+type Tag = {
+    id: string,
+    name: string
+}
 type TagListModel = {
-    data: string[]
-    fetch: () => {}
+    data: Tag[]
+    fetch: () => Tag[]
     create: (name: string) => 'duplicated' | 'success'//success表示成功 duplicated表示失败.。联合类型
     save: () => void
 }
@@ -14,8 +19,12 @@ const tagListModel: TagListModel = {
         return this.data
     },
     create(name) {
-        if (this.data.indexOf(name) >= 0) { return 'duplicated'; }
-        this.data.push(name);
+        //这个时候data是个对象  {"id":"1","name":"1";"id":"2","name":"2"}
+        //没有办法直接this.data.indexOf(name)这样取到
+        //先把所有的name保存到names里面
+        const names = this.data.map(item => item.name)
+        if (names.indexOf(name) >= 0) { return 'duplicated'; }
+        this.data.push({ id: name, name: name });
         this.save();
         return 'success';
     },
