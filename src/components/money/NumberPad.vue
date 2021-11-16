0ae91d2d6792fd39d66750b95e2eca5d6281fe88
aoxiufang <1677120510@qq.com>
2021-11-16 18:56:28 +0800

    完成统计页面

--- src/components/money/NumberPad.vue
+++ src/components/money/NumberPad.vue
@@ -5,80 +5,50 @@
       <button @click="inputContent">1</button>
       <button @click="inputContent">2</button>
       <button @click="inputContent">3</button>
-      <button @click="inputContent" class="add">+</button>
+      <button @click="remove">删除</button>
       <button @click="inputContent">4</button>
       <button @click="inputContent">5</button>
       <button @click="inputContent">6</button>
-      <button @click="inputContent" class="subtract">-</button>
+      <button @click="clear">清空</button>
       <button @click="inputContent">7</button>
       <button @click="inputContent">8</button>
       <button @click="inputContent">9</button>
-      <button @click="clear">清空</button>
-      <button @click="inputContent">.</button>
-      <button @click="inputContent" class="zero">0</button>
-      <button @click="remove">删除</button>
       <button @click="ok" class="ok">OK</button>
+      <button @click="inputContent" class="zero">0</button>
+      <button @click="inputContent">.</button>
     </div>
   </div>
 </template>
 
-<script lang ="ts">
+<script lang="ts">
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
-
 @Component
 export default class NumberPad extends Vue {
-  output: string = "0";
-  input = "";
-  lastbtn: boolean[] = [];
-  flag = false;
+  @Prop(Number) readonly value!: number;
+  output = this.value.toString();
   inputContent(event: MouseEvent) {
-    //点击事件就是一个鼠标事件
-    const button = event.target as HTMLButtonElement; //用as HTMLButtonElement强制指定类型
-    this.input = button.textContent!; //！表示强制指定input不会是空的  也可以写成as string
+    const button = event.target as HTMLButtonElement;
+    const input = button.textContent!;
     if (this.output.length === 16) {
       return;
     }
     if (this.output === "0") {
-      if ("0123456789".indexOf(this.input) >= 0) {
-        this.output = this.input;
+      if ("0123456789".indexOf(input) >= 0) {
+        this.output = input;
       } else {
-        this.output += this.input;
+        this.output += input;
       }
       return;
     }
-
-    if (this.output.indexOf(".") >= 0) {
-      if (this.input === ".") {
-        return;
-      }
+    if (this.output.indexOf(".") >= 0 && input === ".") {
+      return;
     }
-    this.output += this.input;
-    //加减按钮的操作。
-    //当点击数字按钮或者加减运算符按钮时，将值逐一拼接在output中
-    //当连续点击加减运算符按钮时，用第二次的覆盖掉第一次的
-    //为了验证上一次和这一次的是否都是加减运算符，我们将每一次点击的按钮都通过 flag 布尔值记录在 lastbtn 中，只有当点击加减运算符时，将 flag 设置成 true
-    if (this.input === "+" || this.input === "-") {
-      this.flag = true;
-    } else {
-      this.flag = false;
-    }
-    this.lastbtn.push(this.flag);
-    if (
-      this.lastbtn[this.lastbtn.length - 2] &&
-      this.lastbtn[this.lastbtn.length - 1]
-    ) {
-      this.output = this.output.substring(0, this.output.length - 1);
-    }
+    this.output += input;
   }
-
   remove() {
     if (this.output.length === 1) {
-      if (this.output === "0") {
-        return;
-      } else {
-        this.output = "0";
-      }
+      this.output = "0";
     } else {
       this.output = this.output.slice(0, -1);
     }
@@ -87,25 +57,9 @@ export default class NumberPad extends Vue {
     this.output = "0";
   }
   ok() {
-    // if (this.output.indexOf("+") >= 0) {
-    //   const array = this.output.split("+");
-    //   const sum = array.reduce((a, b) => {
-    //     return parseInt(a) + parseInt(b);
-    //   });
-    //   this.output = sum;
-    // } else if (this.output.indexOf("-") >= 0) {
-    //   const array = this.output.split("-");
-    //   const sub = array.reduce((a, b) => {
-    //     return parseInt(a) - parseInt(b);
-    //   });
-    //   console.log(sub);
-    //   this.output = sub;
-    // } else (this.output){
-    //   return this.output;
-    // }
-    this.$emit("update:value", this.output);
-    //当点击ok的时候，把收集起来的数据保存到数据库
-    this.$emit("submit", this.output);
+    const number = parseFloat(this.output);
+    this.$emit("update:value", number);
+    this.$emit("submit", number);
     this.output = "0";
   }
 }
@@ -113,40 +67,60 @@ export default class NumberPad extends Vue {
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
-@import "~@/assets/style/reset.scss";
-
 .numberPad {
   .output {
-    @extend %innerShadow;
     @extend %clearFix;
+    @extend %innerShadow;
     font-size: 36px;
-    font-family: Consolas, monospace; //把字体设成计算机等宽字体
+    font-family: Consolas, monospace;
     padding: 9px 16px;
     text-align: right;
     height: 72px;
   }
   .buttons {
-    @extend %clearFix; //把所有选择器复制到clearFix
-    $bg: #f2f2f2; //要声明在外面
+    @extend %clearFix;
     > button {
       width: 25%;
       height: 64px;
       float: left;
-      background: $bg;
-      border: 1px solid #bdbdbd;
-      border-left: none;
-      border-bottom: none;
+      background: transparent;
+      border: none;
       &.ok {
-        // &符号表示上一级选择器
-        background: $color-highlight;
+        height: 64 * 2px;
+        float: right;
+      }
+      &.zero {
+        width: 25 * 2%;
+      }
+      $bg: #f2f2f2;
+      &:nth-child(1) {
+        background: $bg;
+      }
+      &:nth-child(2),
+      &:nth-child(5) {
+        background: darken($bg, 4%);
+      }
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        background: darken($bg, 4 * 2%);
       }
       &:nth-child(4),
-      &:nth-child(8),
-      &:nth-child(12) {
-        border-right: none;
+      &:nth-child(7),
+      &:nth-child(10) {
+        background: darken($bg, 4 * 3%);
       }
-    }
-    &.class {
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
+        background: darken($bg, 4 * 4%);
+      }
+      &:nth-child(14) {
+        background: darken($bg, 4 * 5%);
+      }
+      &:nth-child(12) {
+        background: darken($bg, 4 * 6%);
+      }
     }
   }
 }
